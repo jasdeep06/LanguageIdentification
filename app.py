7b95381a687e15ebba0dc6b9a13f371196351df9
Jasdeep Singh Chhabra <jasdeepchhabra94@gmail.com>
2017-09-14 04:42:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,3 +25,9 @@ def upload():
     return render_template('upload.html')
 
 
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
